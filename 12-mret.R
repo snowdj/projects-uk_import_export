2ae6c1ef6f8027f6260dd05a58dcfd876df3cd14
luca valnegri <l.valnegri@datamaps.co.uk>
2018-02-21 00:54:09 +0000

    changed name of dt into dts

--- 12-mret.R
+++ 12-mret.R
@@ -7,30 +7,30 @@ pkg <- c('data.table', 'RMySQL')
 invisible(lapply(pkg, require, character = TRUE))
 
 # retrieve data directly from ONS
-dt <- fread(
+dts <- fread(
         'https://www.ons.gov.uk/file?uri=/economy/nationalaccounts/balanceofpayments/datasets/tradeingoodsmretsallbopeu2013timeseriesspreadsheet/current/mret.csv',
         skip = 1
 )
 
-# delete all rows not monthly actual data
-dt <- dt[nchar(CDID) == 8][substr(CDID, 1, 4) >= 1998]
+# delete all rows not monthly or without data
+dts <- dts[nchar(CDID) == 8][substr(CDID, 1, 4) >= 1998]
 
-# recode date as numeric yyyymm  
-dt[, m := substr(CDID, 6, 8)][.(m = unique(m), to = 1:12), on = 'm', m := i.to][, CDID := paste0(substr(CDID, 1, 4), ifelse(nchar(m) == 1, '0', ''), m) ][, m := NULL]
+# recode "date" as numeric yyyymm  
+dts[, m := substr(CDID, 6, 8)][.(m = unique(m), to = 1:12), on = 'm', m := i.to][, CDID := paste0(substr(CDID, 1, 4), ifelse(nchar(m) == 1, '0', ''), m) ][, m := NULL]
 
 # convert to long type
-dt <- melt.data.table(dt, 'CDID', variable.factor = FALSE, na.rm = TRUE)
+dts <- melt.data.table(dts, 'CDID', variable.factor = FALSE, na.rm = TRUE)
 
 # rename molten table
-setnames(dt, c('datefield', 'cdid', 'value'))
+setnames(dts, c('datefield', 'cdid', 'value'))
 
 # delete NA values 
-dt <- dt[!is.na(value)]
+dts <- dts[!is.na(value)]
 
 # save table
 dbc <- dbConnect(MySQL(), group = 'dataOps', dbname = 'uk_import_export')
 dbSendQuery(dbc, "TRUNCATE TABLE mret")
-dbWriteTable(dbc, 'mret', dt[order(datefield, cdid)], append = TRUE, row.names = FALSE)
+dbWriteTable(dbc, 'mret', dts[order(datefield, cdid)], append = TRUE, row.names = FALSE)
 dbDisconnect(dbc)
 
 # clean and exit
