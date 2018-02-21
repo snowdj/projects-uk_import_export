780a7db2f63b4d1c991d13f909567ad22328c583
luca valnegri <l.valnegri@datamaps.co.uk>
2018-02-21 00:53:52 +0000

    changed name of dt into dts

--- 11-mq10.R
+++ 11-mq10.R
@@ -7,30 +7,30 @@ pkg <- c('data.table', 'RMySQL')
 invisible(lapply(pkg, require, character = TRUE))
 
 # retrieve data directly from ONS
-dt <- fread(
+dts <- fread(
         'https://www.ons.gov.uk/file?uri=/businessindustryandtrade/internationaltrade/datasets/uktradeingoodsbyclassificationofproductbyactivity/current/mq10.csv',
         skip = 1        
 )
 
 # delete all rows not quarters data
-dt <- dt[grep('Q', CDID)]
+dts <- dts[grep('Q', CDID)]
 
 # recode quarter as numeric yyyyq
-dt[, CDID := as.numeric(gsub('[^0-9]', '', CDID)) ]
+dts[, CDID := as.numeric(gsub('[^0-9]', '', CDID)) ]
 
 # convert to long type
-dt <- melt.data.table(dt, 'CDID', variable.factor = FALSE, na.rm = TRUE)
+dts <- melt.data.table(dts, 'CDID', variable.factor = FALSE, na.rm = TRUE)
 
 # rename molten table
-setnames(dt, c('datefield', 'cdid', 'value'))
+setnames(dts, c('datefield', 'cdid', 'value'))
 
 # delete NA values 
-dt <- dt[!is.na(value)]
+dts <- dts[!is.na(value)]
 
 # save table
 dbc <- dbConnect(MySQL(), group = 'dataOps', dbname = 'uk_import_export')
 dbSendQuery(dbc, "TRUNCATE TABLE mq10")
-dbWriteTable(dbc, 'mq10', dt[order(datefield, cdid)], append = TRUE, row.names = FALSE)
+dbWriteTable(dbc, 'mq10', dts[order(datefield, cdid)], append = TRUE, row.names = FALSE)
 dbDisconnect(dbc)
 
 # clean and exit
